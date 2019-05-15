f50df3e0e4bfb139c92f04a859b668651638f328
vladc99 <50425387+vladc99@users.noreply.github.com>
2019-05-15 15:50:12 -0400

    Update EmployeeTest.java

--- src/ca/sheridancollege/week1/softwarefundamentals/EmployeeTest.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/EmployeeTest.java
@@ -2,6 +2,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *
+ * LINE ADDED FOR COMMIT NUMBER 3
  */
 package ca.sheridancollege.week1.softwarefundamentals;
 
