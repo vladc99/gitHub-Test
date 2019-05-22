decc7f5edb625db80a3355f00d7e87c17113d5fd
vladc99 <50425387+vladc99@users.noreply.github.com>
2019-05-22 14:55:13 -0400

    Commit #1 from github
    Github commit #1

--- src/ca/sheridancollege/week1/softwarefundamentals/PartTimeStudents.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/PartTimeStudents.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. First Commit from github!
  */
 
 package ca.sheridancollege.week1.softwarefundamentals;
